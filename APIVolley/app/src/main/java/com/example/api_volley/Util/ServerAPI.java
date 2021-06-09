b5276db5526389fae6baece32fa28acc89ffa226
E31190731 <65715169+E31190731@users.noreply.github.com>
2021-06-09 19:43:14 +0700

    Upload

---
+++ APIVolley/app/src/main/java/com/example/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.0.7/ci/index.php/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.0.7/ci/index.php/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.0.7/ci/index.php/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.0.7/ci/index.php/Mahasiswa/ApiUpdate";
+}
